5a885adc2051e8feebcb825ca15b691c04a1b602
mikecirone <michael@ballparq.io>
2017-08-10 21:51:05 -0700

    added base TaskRunners prototype to have all task runners only ever have one task runner that is actively processing its tasks; from user point of view, makes it so that all ajax gets nicely populate and flow down page

--- util/TaskRunner.js
+++ util/TaskRunner.js
@@ -1,24 +1,60 @@
+/*
+  processes all tasks for a single task runner, then moves on to
+  next task runner that has tasks and is in task runner queue
+*/
+
+function TaskRunners() { }
+
+TaskRunners.prototype.taskRunners = []
+TaskRunners.prototype.baseIsProcessing = false
+TaskRunners.prototype.baseProcess = function() {
+    if(!this.baseIsProcessing) {
+        this.baseIsProcessing = true
+        var taskRunner = this.taskRunners.shift()
+        taskRunner.isWaiting = false
+        taskRunner.isProcessing = true
+        taskRunner.process()
+    }
+}
+TaskRunners.prototype.baseAdd = function(taskRunner) {
+    //only called when a task runner is not in line of task runners
+    //waiting their turn to do their tasks
+    this.taskRunners.push(taskRunner)
+    this.baseProcess()
+}
+TaskRunners.prototype.baseDone = function() {
+    this.baseIsProcessing = false
+    if(this.taskRunners.length > 0) {
+        this.baseProcess()
+    }
+}
 
 function TaskRunner() {
     this.tasks = []
     this.isProcessing = false
+    this.isWaiting = false
 }
 
-TaskRunner.prototype.process = function() {
-    if( this.isProcessing === false && this.tasks.length > 0 ) {
-        var fx = this.tasks.shift()
-        this.isProcessing = true
+TaskRunner.prototype = new TaskRunners()
 
-        fx( function() {
-            this.isProcessing = false
+TaskRunner.prototype.process = function() {
+    if( this.tasks.length > 0 ) {
+        var task = this.tasks.shift()
+        task( function() {
             this.process()
         }.bind(this) )
+    } else {
+        this.isProcessing = false
+        this.baseDone()
     }
 }
 
 TaskRunner.prototype.push = function(task) {
     this.tasks.push(task)
-    this.process()
+    if(!this.isWaiting && !this.isProcessing) {
+        this.isWaiting = true
+        this.baseAdd(this)
+    }
 }
 
 module.exports = TaskRunner
